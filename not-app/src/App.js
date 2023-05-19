987bcff591fe7760a02919729d8abf1d5510cd8c
Megan <megantech@hotmail.com>
2023-05-19 15:43:22 +0000

    NavBar created as react component

---
+++ not-app/src/App.js
@@ -0,0 +1,14 @@
+import css from './App.module.css';
+import NavBarSimple from './components/NavBarSimple';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className={css.App}>
+      <Sidebar />
+      <NavBarSimple />
+    </div>
+  );
+}
+
+export default App;
