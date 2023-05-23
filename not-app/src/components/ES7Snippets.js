ab9178442bbcda4a5861e5243e914a92f697874c
Megan <megantech@hotmail.com>
2023-05-23 09:17:32 +0000

    Add: stylin updates to NavBar login Button

---
+++ not-app/src/components/ES7Snippets.js
@@ -0,0 +1,53 @@
+// 'rfce' --> creates and exports functional component
+
+// import React from 'react'
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// 'rafce' --> creates  and exports arrow function component 
+
+// import React from 'react'
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// 'rce' --> Creates and exports class component
+
+//import React, { Component } from 'react'
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+// 'rconst' --> creates constructor
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+// 'clg' --> logs to console
+
+// console.log(first)
+
+// 'clo' - log and object and its name as a String. 
+
+// console.log('first', first)
