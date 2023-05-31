87d74d876b9647cd08b4f380ac30f5693a66d2f8
Megan <megantech@hotmail.com>
2023-05-31 12:05:13 +0000

    Created rendered List and child component to display images

--- not-app/src/App.js
+++ not-app/src/App.js
@@ -1,12 +1,16 @@
 import css from './App.module.css';
 import NavBarSimple from './components/NavBarSimple';
+import NavBarForm from './components/NavBarForm';
 import Sidebar from './components/Sidebar';
+import Content from './components/Content';
+
 
 function App() {
   return (
     <div className={css.App}>
+      <NavBarForm />
       <Sidebar />
-      <NavBarSimple />
+      <Content />
     </div>
   );
 }
